f4152b1e3183f648ba62ba936a360e7641910891
Spenser Black <spenserblack01@gmail.com>
2022-06-29 14:54:21 -0400

    Implement grouping

--- src/print_extended/__init__.py
+++ src/print_extended/__init__.py
@@ -10,7 +10,16 @@ print("Hello, world!")
 """
 import sys
 
-__all__ = ["Printer", "print", "eprint"]
+__all__ = ["Printer", "print", "eprint", "NoGroupError"]
+
+
+class NoGroupError(Exception):
+    """
+    Raised when the user attempts to end a group without starting one.
+    """
+
+    def __init__(self):
+        super().__init__("Cannot end a group without starting one.")
 
 
 class Printer:
@@ -20,19 +29,37 @@ class Printer:
 
     _print = print
 
-    def __init__(self, file=sys.stdout):
+    def __init__(self, group_indent="    ", file=sys.stdout):
         """
         Initialize the printer.
 
         # Args
 
+        - `group_indent`: The indentation for each group of items.
         - `file`: The file to print to.
         """
         self.file = file
+        self.group_indent = group_indent
+        self.group_level = 0
 
     def __call__(self, *args, **kwargs):
         file = kwargs.pop("file", self.file)
-        return self._print(file=file, *args, **kwargs)
+        self._print(self.group_indent * self.group_level, file=file, end="")
+        self._print(file=file, *args, **kwargs)
+
+    def group(self):
+        """
+        Starts a group by increasing the indentation level.
+        """
+        self.group_level += 1
+
+    def endgroup(self):
+        """
+        Ends a group by decreasing the indentation level.
+        """
+        if self.group_level == 0:
+            raise NoGroupError()
+        self.group_level -= 1
 
 
 print = Printer()
--- tests/test_print_extended.py
+++ tests/test_print_extended.py
@@ -8,7 +8,7 @@ class TestPrinter(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.out = StringIO()
-        cls.print = Printer(cls.out)
+        cls.print = Printer(file=cls.out)
 
     def tearDown(self):
         self.out.truncate(0)
@@ -20,3 +20,28 @@ class TestPrinter(TestCase):
         """
         self.print("Hello, world!")
         self.assertEqual(self.out.getvalue(), "Hello, world!\n")
+
+    def test_grouping(self):
+        """
+        It should create groups.
+        """
+        self.print("no group")
+        self.print.group()
+        self.print("group 1")
+        self.print("group 1 line 2")
+        self.print.group()
+        self.print("group 2")
+        self.print.endgroup()
+        self.print.endgroup()
+        self.print("no group")
+
+        self.assertEqual(
+            self.out.getvalue().splitlines(),
+            [
+                "no group",
+                "    group 1",
+                "    group 1 line 2",
+                "        group 2",
+                "no group",
+            ],
+        )
